1a4dbf0cedde3812ec55bab6f9a0a560c2750ca0
hirayama <hirayama@bp.horizon.co.jp>
2017-03-08 18:38:44 +0900

    app.module 修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { BusyoFormComponent } from './busyo-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    BusyoFormComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
