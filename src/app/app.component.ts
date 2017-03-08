89a1b6af1132fb5dcd3a6d8c0bfb5d903a151536
hirayama <hirayama@bp.horizon.co.jp>
2017-03-08 18:40:57 +0900

    app.component 修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { Busyo } from './busyo';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>はじめまして {{name}} !</h1>
-    <p>{{name}}って難しそうですけどがんばるぞ</p>`,
+    <busyo-form></busyo-from>
+    `,
 })
 export class AppComponent  { 
   name = 'Angular';
-
-  myBusyo = new Busyo(2,'山','大火事');
 }
