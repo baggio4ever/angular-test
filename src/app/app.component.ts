64b21dd73a8560abef81200dc14ede56e1be3719
hirayama <hirayama@bp.horizon.co.jp>
2017-03-08 17:30:13 +0900

    更に小修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>はじめまして {{name}}</h1>`,
+  template: `
+    <h1>はじめまして {{name}} !</h1>
+    <p>{{name}}って難しそうですけどがんばるぞ</p>`,
 })
 export class AppComponent  { name = 'Angular'; }
