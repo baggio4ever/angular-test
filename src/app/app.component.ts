9f8fb8dae86e84ae1866eb6410d545fd7c7d721c
hirayama <hirayama@bp.horizon.co.jp>
2017-03-09 12:06:30 +0900

    タイプミス修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>はじめまして {{name}} !</h1>
-    <busyo-form></busyo-from>
+    <busyo-form></busyo-form>
     `,
 })
 export class AppComponent  { 
